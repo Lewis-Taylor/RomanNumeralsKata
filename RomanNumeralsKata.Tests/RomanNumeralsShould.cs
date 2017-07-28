40abc5cf98016339bddae8bbf2950bb9b8914e25
lewis taylor <lewis.taylor@asos.com>
2017-07-28 11:56:45 +0100

    Implementation of roman numerals Kata

---
+++ RomanNumeralsKata.Tests/RomanNumeralsShould.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace RomanNumeralsKata.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsShould
+    {
+        [TestCase(1,"I")]
+        [TestCase(2,"II")]
+        [TestCase(3,"III")]
+        [TestCase(4,"IV")]
+        [TestCase(5,"V")]
+        [TestCase(6,"VI")]
+        [TestCase(7,"VII")]
+        [TestCase(8,"VIII")]
+        [TestCase(9,"IX")]
+        [TestCase(10,"X")]
+        [TestCase(11,"XI")]
+        [TestCase(12,"XII")]
+        [TestCase(13,"XIII")]
+        [TestCase(14,"XIV")]
+        [TestCase(15,"XV")]
+        [TestCase(40,"XL")]
+        [TestCase(41,"XLI")]
+        [TestCase(42,"XLII")]
+        [TestCase(45,"XLV")]
+        [TestCase(50,"L")]
+        [TestCase(51,"LI")]
+        [TestCase(52,"LII")]
+        [TestCase(55,"LV")]
+        [TestCase(70,"LXX")]
+        public void ReturnRomanNumeral_WhenConverting_GivenTheInputIsAnArabicNumber(int arabicNumber, string expectedRomanNumeral)
+        {
+            var romanNumeral = RomanNumerals.Convert(arabicNumber);
+            romanNumeral.Should().Be(expectedRomanNumeral);
+        }
+    }
+}
---
+++ RomanNumeralsKata/RomanNumerals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumeralsKata
+{
+    public static class RomanNumerals
+    {
+        private static readonly Dictionary<int, string> ArabicToRomanNumeral= new Dictionary<int, string>
+        {
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+
+        public static string Convert(int number)
+        {
+            foreach ( var arabic in ArabicToRomanNumeral.Keys)
+            {
+                if (number >= arabic)
+                {
+                    return ArabicToRomanNumeral[arabic] + Convert(number - arabic);
+                }
+            }
+            
+            return string.Empty;
+        }
+    }
+}
